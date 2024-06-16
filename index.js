e130220e5ca495078ea7bbeade57e3b00b763afd
devashish <mshubh612@gmail.com>
2024-06-16 15:21:03 +0530

    added register route and controller

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 
 import dotenv from 'dotenv';
 import connectDB from './db/index.js';
+import express from "express";
 
 dotenv.config()
 const app = express();
 const port = process.env.PORT;
 const dbUrl = process.env.MONGODB_URI;
 
-console.log(port)
+console.log(`server is listining to the port ${port}`)
 console.log(dbUrl)
 
 connectDB().then(()=>{
