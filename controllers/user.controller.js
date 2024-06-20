2a1e5db77f0b30c8cf5ae6bf84782f4e7a6faf48
devashish <mshubh612@gmail.com>
2024-06-20 17:16:22 +0530

    add user account management endpoints

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -204,12 +204,126 @@ const refreshAccessToken = asyncHandler(async(req, res) => {
     }
 })
 
+const changeCurrentPassword = asyncHandler(async(req, res) => {
+    const {oldPassword, newPassword} = req.body
+
+    const user = await User.findById(req.user?._id)
+    const isPasswordCorrect = await user.
+    isPasswordCorrect(oldPassword)
+
+    if(!isPasswordCorrect) {
+        throw new ApiError(400, "Invalid Old Password")
+    }
+    
+    user.password = newPassword
+    await user.save({validateBeforeSave: false})
+})
+
+const getCurrentUser = asyncHandler(async(req, res) => {
+    return res
+    .status(200)
+    .json(200, req.user, "current user fetched successfully")
+})
+
+const updateAccountDetails = asyncHandler(async(req, res) => {
+    const {fullname, email} = req.body
+
+    if (!fullname || !email) {
+        throw new ApiError(400, "All fields are required")
+    }
+
+    const user = User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                fullname,
+                email
+            }
+        },
+        {new: true}
+    ).select("-password")
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, user, "Account details updated Successfully"))
+})
+
+const updateUserAvatar = asyncHandler(async(req, res) => {
+    const avatarLocalPath = req.file?.path
+
+    if (!avatarLocalPath) {
+        throw new ApiError(400,"Avatar file is missing")
+    }
+    
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+
+    if (!avatar.url) {
+        throw new ApiError(400, "Error while uploading on avatar")
+    }
+    
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                avatar: avatar.url
+            }
+        },
+        {
+            new: true //will show updated details
+        }
+    ).select("-password")
+
+        return res
+        .status(200)
+        .json(
+        new ApiResponse(200, user, "avatar image updated successfully")
+    )   
+
+}) 
+
+const updateUserCoverImage = asyncHandler(async(req, res) => {
+    const coverLocalPath = req.file?.path
+
+    if (!coverLocalPath) {
+        throw new ApiError(400,"cover image file is missing")
+    }
+    
+    const coverImage = await uploadOnCloudinary(coverLocalPath)
+
+    if (!coverImage.url) {
+        throw new ApiError(400, "Error while uploading on avatar")
+    }
+    
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                coverImage: coverImage.url
+            }
+        },
+        {
+            new: true //will show updated details
+        }
+    ).select("-password")
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, user, "covre image updated successfully")
+    )
+
+})
 
 
 export { registerUser,
          loginUser,
          logoutUser,
-         refreshAccessToken
+         refreshAccessToken,
+         changeCurrentPassword,
+         getCurrentUser,
+         updateAccountDetails,
+         updateUserAvatar,
+         updateUserCoverImage
 
  };
 
--- middleware/multer.middleware.js
+++ middleware/multer.middleware.js
@@ -13,4 +13,3 @@ const storage = multer.diskStorage({
     storage,
    })
 
-   //is this a different project ? yes its different from the lectures its my own personal one, what is that topic? whole backend node and express . the error is in your multers callback function which is get by multer....ok so please resolve it . what you exactly want to do by that request tell me that first ..ok..i want to regidter the user you can see my bussiness logic of the register user
